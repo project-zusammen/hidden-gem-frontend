903c5d2471047caa77dafa79ce8e1ace3cd149ad
Fatma Janna <fatma.janna@quinyx.com>
2024-01-21 06:19:40 +0700

    remove unnecesarry package and comment

--- src/components/Footer/index.test.jsx
+++ src/components/Footer/index.test.jsx
@@ -1,13 +1,13 @@
 // footer/index.test.jsx
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; // for additional matchers
+// import '@testing-library/jest-dom/extend-expect'; 
 import Footer from './index';
 
 describe('Footer Component', () => {
   test('renders Footer component', () => {
     render(<Footer />);
-    // You can add specific queries or assertions based on your component structure
+   
     expect(screen.getByText(/Crafted with passion by Team Evolvy/)).toBeInTheDocument();
     expect(screen.getByText(/Home/)).toBeInTheDocument();
     expect(screen.getByText(/Contact Us/)).toBeInTheDocument();
