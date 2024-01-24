91ab12318a95ee262aa5f25f74df4473ddccac1c
anangyoga <aharwinto@gmail.com>
2024-01-24 14:56:19 +0700

    added React import

--- src/components/Upvotes/index.jsx
+++ src/components/Upvotes/index.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import React from "react";
 import { IconButton, Typography } from "@mui/material";
 import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
 import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
