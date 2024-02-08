c605a5f8baecd40f4e6b05052b6c588056574e8e
Fatma Janna <fatma.janna@quinyx.com>
2024-02-08 07:20:54 +0700

    fix failed tests

--- src/components/Card/index.test.jsx
+++ src/components/Card/index.test.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import RecipeReviewCard from './index';
+import { BrowserRouter as Router } from "react-router-dom";
 
 test('renders RecipeReviewCard component', () => {
-  render(<RecipeReviewCard />);
+  render(
+    <Router> 
+      <RecipeReviewCard />
+    </Router>
+  );
   expect(screen.getByText(/Shrimp and Chorizo Paella/i)).toBeInTheDocument();
   expect(screen.getByText(/September 14, 2016/i)).toBeInTheDocument();
 });
 
 test('expands/collapses when clicking on "ExpandMore" button', () => {
-  render(<RecipeReviewCard />);
+  render(
+    <Router> 
+      <RecipeReviewCard />
+    </Router>
+  );
   expect(screen.queryByText(/Method:/i)).not.toBeInTheDocument();
 });
--- src/pages/Homepage.test.jsx
+++ src/pages/Homepage.test.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import Homepage from "./Homepage";
 import { render, screen } from "@testing-library/react";
+import Homepage from "./Homepage";
+import { BrowserRouter as Router } from "react-router-dom";
 
 describe("Homepage", () => {
-  test("renders Homepage", () => {
-    render(<Homepage />);
-  });
-
   test("renders left, center, and right columns", () => {
-    render(<Homepage />);
+    render(
+      <Router>
+        <Homepage />
+      </Router>
+    );
 
     expect(screen.getByText(/Left Column/)).toBeInTheDocument();
     expect(screen.getByText(/Right Column/)).toBeInTheDocument();
