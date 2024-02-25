4693aa160eb0a8fbaa200feaa26fa65a5810cb1b
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2024-02-25 10:18:24 +0700

    refactor: card to carousel

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,61 +1,72 @@
-import React from 'react';
-import { Grid, Typography } from '@mui/material';
-import Card from '../Card';
+import React from "react";
+// import { Box, Container, Grid, Typography } from "@mui/material";
+import Card from "../Card";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Body = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  };
   return (
-    <Grid container spacing={3} marginTop={5} marginBottom={13}>
-      {/* Main Content Sections */}
-      <Grid item xs={4} md={4} lg={2}>
-      </Grid>
-      <Grid item xs={4} md={4} lg={8}>
-      </Grid>
-      <Grid item xs={4} md={4} lg={2}>
-      </Grid>
-      <Grid item xs={4} md={4} lg={2}>
-      </Grid>
-      <Grid item xs={4} md={4} lg={8}>
-        <Typography variant="h1" gutterBottom textAlign={"center"}>
-          New Reviews
-        </Typography>
-      </Grid>
-      <Grid item xs={4} md={4} lg={2}>
-      </Grid>
-      <Grid item xs={4} md={4} lg={2}>
-      </Grid>
-      <Grid item xs={4} md={4} lg={8}>
-        <Grid container spacing={2} xs={12}>
-          <Grid item xs={3}>
-            <Card>
-              {/* Card content here */}
-            </Card>
-          </Grid>
-          <Grid item xs={3}>
-            <Card>
-              {/* Card content here */}
-            </Card>
-          </Grid>
-          <Grid item xs={3}>
-            <Card>
-              {/* Card content here */}
-            </Card>
-          </Grid>
-          <Grid item xs={3}>
-            <Card>
-              {/* Card content here */}
-            </Card>
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item xs={4} md={4} lg={2}>
-      </Grid>
-      <Grid item xs={4} md={4} lg={2}>
-      </Grid>
-      <Grid item xs={4} md={4} lg={8}>
-      </Grid>
-      <Grid item xs={4} md={4} lg={2}>
-      </Grid>
-    </Grid>
+    // <Grid container spacing={3} marginTop={5} marginBottom={13}>
+    //   {/* Main Content Sections */}
+    //   <Grid item xs={4} md={4} lg={2}></Grid>
+    //   <Grid item xs={4} md={4} lg={8}></Grid>
+    //   <Grid item xs={4} md={4} lg={2}></Grid>
+    //   <Grid item xs={4} md={4} lg={2}></Grid>
+    //   <Grid item xs={4} md={4} lg={8}>
+    //     <Typography variant="h1" gutterBottom textAlign={"center"}>
+    //       New Posts
+    //     </Typography>
+    //   </Grid>
+    //   <Grid item xs={4} md={4} lg={2}></Grid>
+    //   <Grid item xs={4} md={4} lg={2}></Grid>
+    //   <Grid item xs={4} md={4} lg={8}>
+    //     <Grid container spacing={2} xs={12}>
+    //       <Grid item xs={3}>
+    //         <Card>{/* Card content here */}</Card>
+    //       </Grid>
+    //       <Grid item xs={3}>
+    //         <Card>{/* Card content here */}</Card>
+    //       </Grid>
+    //       <Grid item xs={3}>
+    //         <Card>{/* Card content here */}</Card>
+    //       </Grid>
+    //     </Grid>
+    //   </Grid>
+    //   <Grid item xs={4} md={4} lg={2}></Grid>
+    //   <Grid item xs={4} md={4} lg={2}></Grid>
+    //   <Grid item xs={4} md={4} lg={8}></Grid>
+    //   {/* <Grid item xs={4} md={4} lg={2}></Grid> */}
+    // </Grid>
+    <>
+      <h1 className="body-post">New Posts</h1>
+      <div className="slider-container">
+        <Slider {...settings}>
+          <div>
+            <Card />
+          </div>
+          <div>
+            <Card />
+          </div>
+          <div>
+            <Card />
+          </div>
+          <div>
+            <Card />
+          </div>
+          <div>
+            <Card />
+          </div>
+        </Slider>
+      </div>
+    </>
   );
 };
 
