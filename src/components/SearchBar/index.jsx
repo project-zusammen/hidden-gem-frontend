918d08b48e1f3c6501a5ed96dcf97e9995a1a2e2
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2024-02-25 10:22:00 +0700

    style: change color

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,6 +1,14 @@
-import React, { useState } from 'react';
-import { Tab, Tabs, TextField, Box, IconButton } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
+import React, { useState } from "react";
+import {
+  Tab,
+  Tabs,
+  TextField,
+  Box,
+  IconButton,
+  Button,
+  InputAdornment,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBarWithTabs = () => {
   const [currentTab, setCurrentTab] = useState(0);
@@ -10,8 +18,23 @@ const SearchBarWithTabs = () => {
   };
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '50px', marginBottom: '50px' }}>
-      <Tabs value={currentTab} onChange={handleChange} aria-label="Search tabs">
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "50px",
+        marginBottom: "50px",
+      }}
+    >
+      <Tabs
+        value={currentTab}
+        onChange={handleChange}
+        aria-label="Search tabs"
+        // textColor="neutral"
+        // indicatorColor="neutral.dark"
+      >
         <Tab label="Food" />
         <Tab label="Hotel" />
         <Tab label="Cafe" />
@@ -19,11 +42,25 @@ const SearchBarWithTabs = () => {
         <Tab label="Entertainment" />
         <Tab label="Shopping" />
       </Tabs>
-      <Box sx={{ display: 'flex', alignItems: 'center', marginTop: 2 }}>
-        <TextField label="Search" size="small"/>
-        <IconButton type="submit" aria-label="search">
-          <SearchIcon />
-        </IconButton>
+      <Box sx={{ display: "flex", alignItems: "center", marginTop: 2 }}>
+        <TextField
+          label="Search"
+          size="small"
+          sx={{ width: 600 }}
+          InputProps={{
+            sx: { borderRadius: 20, height: "45px" },
+            endAdornment: (
+              <InputAdornment position="end">
+                <Button
+                  variant="contained"
+                  sx={{ borderRadius: 20, marginRight: "-5px" }}
+                >
+                  Search
+                </Button>
+              </InputAdornment>
+            ),
+          }}
+        />
       </Box>
     </Box>
   );
