8c55e51059f75e0c4af74b811fe543fc29c2d6e1
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2024-03-04 09:48:47 +0700

    feat: react-slick-responsive

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -5,45 +5,41 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Body = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
+  var settings = {
+    dots: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 390,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
-    // <Grid container spacing={3} marginTop={5} marginBottom={13}>
-    //   {/* Main Content Sections */}
-    //   <Grid item xs={4} md={4} lg={2}></Grid>
-    //   <Grid item xs={4} md={4} lg={8}></Grid>
-    //   <Grid item xs={4} md={4} lg={2}></Grid>
-    //   <Grid item xs={4} md={4} lg={2}></Grid>
-    //   <Grid item xs={4} md={4} lg={8}>
-    //     <Typography variant="h1" gutterBottom textAlign={"center"}>
-    //       New Posts
-    //     </Typography>
-    //   </Grid>
-    //   <Grid item xs={4} md={4} lg={2}></Grid>
-    //   <Grid item xs={4} md={4} lg={2}></Grid>
-    //   <Grid item xs={4} md={4} lg={8}>
-    //     <Grid container spacing={2} xs={12}>
-    //       <Grid item xs={3}>
-    //         <Card>{/* Card content here */}</Card>
-    //       </Grid>
-    //       <Grid item xs={3}>
-    //         <Card>{/* Card content here */}</Card>
-    //       </Grid>
-    //       <Grid item xs={3}>
-    //         <Card>{/* Card content here */}</Card>
-    //       </Grid>
-    //     </Grid>
-    //   </Grid>
-    //   <Grid item xs={4} md={4} lg={2}></Grid>
-    //   <Grid item xs={4} md={4} lg={2}></Grid>
-    //   <Grid item xs={4} md={4} lg={8}></Grid>
-    //   {/* <Grid item xs={4} md={4} lg={2}></Grid> */}
-    // </Grid>
     <>
       <h1 className="body-post">New Reviews</h1>
       <div className="slider-container">
