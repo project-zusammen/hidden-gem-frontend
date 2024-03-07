a5b6f326c74a77b49c28d720eec1c8ca4736e43a
anangyoga <aharwinto@gmail.com>
2024-03-07 10:17:21 +0700

    fix: api request with axios

--- src/api/region.js
+++ src/api/region.js
@@ -1,10 +1,10 @@
+import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getRegion = async () => {
   try {
-    const response = await fetch(`${API_BASE_URL}/region`);
-    const data = await response.json();
-    return data.data;
+    const response = await axios.get(`${API_BASE_URL}/region`);
+    return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error; // You may handle errors differently based on your application needs
