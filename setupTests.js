935218e377bff4ba1b049829c2049763572565ac
Fatma Janna <fatma.janna@quinyx.com>
2024-03-07 11:03:02 +0700

    keep for now

---
+++ setupTests.js
@@ -0,0 +1,1 @@
+import 'esbuild-register';
--- src/api/review.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-import Body from "../components/Body";
-
-test("uses API_URL from .env.test", () => {
-  process.env.VITE_API_BASE_URL = "https://hiddengem.pythonanywhere.com/api";
-  const { getByText } = render(<Body />);
-});
