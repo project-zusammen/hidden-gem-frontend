0a3bcfa0b6d0b9476c7ed19b76a317247ba7a06a
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2024-03-10 16:21:44 +0700

    style: add css link

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -3,6 +3,7 @@ import Card from "../Card";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./index.css";
 
 const Body = () => {
   var settings = {
