ec320fd4276908484c16a3758383e5ad4957a36f
Fatma Janna <fatma.janna@quinyx.com>
2024-03-14 14:27:35 +0700

    adjust breakpoints

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -16,7 +16,7 @@ const Body = () => {
     initialSlide: 0,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1280,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
@@ -25,7 +25,7 @@ const Body = () => {
         },
       },
       {
-        breakpoint: 768,
+        breakpoint: 960,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
@@ -33,7 +33,7 @@ const Body = () => {
         },
       },
       {
-        breakpoint: 390,
+        breakpoint: 750,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
