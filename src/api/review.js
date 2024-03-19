6441ddc43503f8d4880c4d2fd8f5d2d296e4da72
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2024-03-19 09:08:53 +0700

    refactor getReview

--- src/api/review.js
+++ src/api/review.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.API_BASE_URL;
-
 export const getReview = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}/review`);
+    const response = await axios.get(`${API_BASE_URL}/review`);
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
